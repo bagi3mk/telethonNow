fe5a350e07e6ddab8e478be7ac9346b6eed39cf1
laksis1500 <64811139+laksis1500@users.noreply.github.com>
2021-10-24 09:42:38 -0700

    Update بحوثات يوتيوب.py

--- userbot/plugins/بحوثات يوتيوب.py
+++ userbot/plugins/بحوثات يوتيوب.py
@@ -77,7 +77,7 @@ async def _(event):
             return "error"
     try:
         "انمي"
-        reply = await event.get_reply_message().message
+        reply = await event.get_reply_message().text
         print(reply)
         splt = reply.split(" ")
         name = splt[1]
